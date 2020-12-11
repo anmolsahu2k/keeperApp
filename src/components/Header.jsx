61446f651f240ef48971e513c49e34f1a3b0cc71
Anmol Sahu <anmolsahu2k@gmail.com>
2020-12-11 20:38:41 +0530

    Add comments in Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 import HighlightIcon from "@material-ui/icons/Highlight";
 
+//define function component 'Header' to be used in 'App.jsx' 
 function Header() {
+    //return the HTML to be rendered
     return (
         <header>
       <h1>
+      	{/*Function component from material-ui*/}
         <HighlightIcon />
         Keeper
       </h1>
@@ -12,4 +15,5 @@ function Header() {
     );
 }
 
+//export 'Header' function component as default export
 export default Header;
