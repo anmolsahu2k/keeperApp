35f9a065f5f864aca8e19ccc1163ecee13d87d54
Anmol Sahu <anmolsahu2k@gmail.com>
2020-12-11 20:37:00 +0530

    Add comments in Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
+
+//define function component 'Footer' to be used in 'App.jsx'  
 function Footer() {
+    //get current year from Date object
     const year = new Date().getFullYear();
+    //return the HTML to be rendered
     return (
         <footer>
       <p>Copyright ⓒ {year}</p>
@@ -9,4 +13,5 @@ function Footer() {
     );
 }
 
+//export 'Footer' function component as default export
 export default Footer;
