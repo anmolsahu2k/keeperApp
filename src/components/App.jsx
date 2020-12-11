a01f05ee67fddb51db6ce612e1a0bd2dfd654201
Anmol Sahu <anmolsahu2k@gmail.com>
2020-12-11 19:17:11 +0530

    Add comments in App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,19 +1,28 @@
+//import React and useState from npm-installed react module
 import React, { useState } from "react";
+//import 'Header' function component from 'Header.jsx' file
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
 import CreateArea from "./CreateArea";
 
+//defining function component 'App' to be used in 'index.js'
 function App() {
+    //state varaible 'notes' is initialized as an empty array denoting the current state value 
+    //state function 'setNotes' lets you update state variable
     const [notes, setNotes] = useState([]);
 
+    //function to add new note to 'notes' array
     function addNote(newNote) {
+        //update state variable 'notes' and add a new note 'newNote' at the end
         setNotes(prevNotes => {
             return [...prevNotes, newNote];
         });
     }
 
+    //function to delete existing note from 'notes' array using 'id'(index of note)
     function deleteNote(id) {
+        //update state variable 'notes' and delete note by matching id using .filter()
         setNotes(prevNotes => {
             return prevNotes.filter((noteItem, index) => {
                 return index !== id;
@@ -21,14 +30,21 @@ function App() {
         });
     }
 
+    //return the HTML to be rendered
     return (
         <div>
+      {/*Header function component*/}
       <Header />
+      {/*CreateArea function component*/}
+      {/*onAdd attribute is passed to the component as a single object called 'props'*/}
       <CreateArea onAdd={addNote} />
+      {/*state variable 'notes' is iterated and callback function is called for each of the array items*/}
       {notes.map((noteItem, index) => {
         return (
+          //Note function component
           <Note
-            key={index}
+            //attributes passed as a single object 'props'
+            key={index}//key attribute required
             id={index}
             title={noteItem.title}
             content={noteItem.content}
@@ -36,9 +52,11 @@ function App() {
           />
         );
       })}
+      {/*Footer function component */}
       <Footer />
     </div>
     );
 }
 
+//export 'App' function component as default export
 export default App;
